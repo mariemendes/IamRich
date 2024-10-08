56a99b9e513efc61021234d6d2435f7411f55aa8
mariemendes <marianemendes1104@gmail.com>
2024-10-08 15:40:27 -0700

    Initial Commit

---
+++ I am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by Mariane Oliveira on 2024-10-08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
